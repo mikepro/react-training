a57e5eda95cf60f0adc52a2f91e426a0c74d90f2
Michael Watson <michael.watson@bglgroup.co.uk>
2019-03-05 12:57:57 +0000

    adding second example jsx

---
+++ Examples/example2/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let MyComponet = ()=>{
+    return <h1>Hello from react</h1>
+}
+
+ReactDOM.render(<MyComponet/>, document.getElementById('root'));
