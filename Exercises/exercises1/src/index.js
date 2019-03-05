1c3d3debe508592318b8a091eabceaa020541797
Michael Watson <michael.watson@bglgroup.co.uk>
2019-03-05 01:01:28 +0000

    Adding the starting point for the first example

---
+++ Exercises/exercises1/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let MyUnorderedList = ()=> {
+    return React.createElement("changeme",null,null)
+};
+
+ReactDOM.render(<MyUnorderedList/>, document.getElementById('root'));
