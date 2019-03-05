d08cb654c9396cee5a40491ab0e73d9acbbec3b9
Michael Watson <michael.watson@bglgroup.co.uk>
2019-03-05 12:46:18 +0000

    Ammending previous example to show variable

--- Examples/example1/src/index.js
+++ Examples/example1/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let MyComponet = ()=> {
-    return React.createElement("h1",null,"Hello from react")
-};
+let MyComponet = React.createElement("h1",null,"Hello from react")
 
-ReactDOM.render(<MyComponet/>, document.getElementById('root'));
+
+ReactDOM.render(MyComponet, document.getElementById('root'));
