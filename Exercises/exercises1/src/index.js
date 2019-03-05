6db571dd865833715a36e04338eec18df0da58ff
Michael Watson <michael.watson@bglgroup.co.uk>
2019-03-05 01:05:22 +0000

    Completed exercise 1

--- Exercises/exercises1/src/index.js
+++ Exercises/exercises1/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 let MyUnorderedList = ()=> {
-    return React.createElement("changeme",null,null)
+    return React.createElement("ul",null,
+        [React.createElement("li",null, 'item 1'),
+        React.createElement("li",null, 'item 2')]
+    )
 };
 
 ReactDOM.render(<MyUnorderedList/>, document.getElementById('root'));
